dbffb868406929cf37e2e6e5775d09d3ee65f7a1
Yassin-hagenimana <hyassin509@gmail.com>
2022-06-05 01:33:20 +0200

    navbar done

--- materialui/src/components/SideBar.jsx
+++ materialui/src/components/SideBar.jsx
@@ -1,11 +1,94 @@
-import { Box } from '@mui/material'
+import { AccountBox, DarkMode, Group, Home, MobileFriendlySharp, Pages, PeopleAlt, Settings, Storefront } from '@mui/icons-material'
+import { Box, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Switch } from '@mui/material'
 import React from 'react'
 
 const SideBar = () => {
   return (
-    <Box bgcolor="yellow" flex={1} p={2} m={1}
+    <Box
+       flex={1}
+        p={2}
+        m={1}
     sx={{display:{xs:"none",sm:"block"}}}
-    >SideBar</Box>
+    >
+        <List>
+            <ListItem disablePadding>
+                <ListItemButton component="a" href="#home">
+                    <ListItemIcon>
+                       <Home/>
+                    </ListItemIcon>
+                    <ListItemText primary="Homepage"/>
+                </ListItemButton>
+
+            </ListItem>
+
+
+            <ListItem disablePadding>
+                <ListItemButton component="a" href="#simple-list">
+                    <ListItemIcon>
+                       <Settings/>
+                    </ListItemIcon>
+                    <ListItemText primary="Settings"/>
+                </ListItemButton>
+
+            </ListItem>
+
+            <ListItem disablePadding>
+                <ListItemButton component="a" href="#simple-lst">
+                    <ListItemIcon>
+                       <AccountBox/>
+                    </ListItemIcon>
+                    <ListItemText primary="Profile"/>
+                </ListItemButton>
+
+            </ListItem>
+            <ListItem disablePadding>
+                <ListItemButton component="a" href="#simple-lst">
+                    <ListItemIcon>
+                       <Storefront/>
+                    </ListItemIcon>
+                    <ListItemText primary="Marketplace"/>
+                </ListItemButton>
+
+            </ListItem>
+            <ListItem disablePadding>
+                <ListItemButton component="a" href="#simple-lst">
+                    <ListItemIcon>
+                       <Pages/>
+                    </ListItemIcon>
+                    <ListItemText primary="Pages"/>
+                </ListItemButton>
+
+            </ListItem>
+            <ListItem disablePadding>
+                <ListItemButton component="a" href="#simple-lst">
+                    <ListItemIcon>
+                       <PeopleAlt/>
+                    </ListItemIcon>
+                    <ListItemText primary="Friends"/>
+                </ListItemButton>
+
+            </ListItem>
+            <ListItem disablePadding>
+                <ListItemButton component="a" href="#simple-lst">
+                    <ListItemIcon>
+                       <Group/>
+                    </ListItemIcon>
+                    <ListItemText primary="Groups"/>
+                </ListItemButton>
+
+            </ListItem>
+
+            <ListItem disablePadding>
+                <ListItemButton component="a" href="#simple-lst">
+                    <ListItemIcon>
+                       <DarkMode/>
+                    </ListItemIcon>
+                    <Switch/> 
+                </ListItemButton>
+
+            </ListItem>
+        </List>
+        </Box>
   )
 }
 
